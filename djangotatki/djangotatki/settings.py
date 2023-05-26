b7b0668eab908daf4245154ab122fd1645d702bb
Marcel Mikulski <marcel.mikulski@wp.pl>
2023-05-26 10:47:39 +0200

    model notatki
    dodanie modelu notatki

--- djangotatki/djangotatki/settings.py
+++ djangotatki/djangotatki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9kqca)+!&w@$3s5t0ge3(wr#g3&olfp95-guiz1xi45f+4(mv#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
