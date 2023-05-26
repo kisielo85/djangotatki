a05daec0b1f3cc30034a474c06778ded698dd2d7
Przemek B <pprzemek89@gmail.com>
2023-05-26 13:31:22 +0200

    wyświetlanie notatek
    mamo bierz aparat ja umiem komputer omg

--- notatki/views.py
+++ notatki/views.py
@@ -1,4 +1,10 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Notatka
 
 def index(request):
-    return HttpResponse("eoo brrrr index brrr")
+    notatki = Notatka.objects.all()
+    return render(request, "index.html", {'notatki': notatki})
+
+def allNotes(request):
+    return HttpResponse("eo vieww")
